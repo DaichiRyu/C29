87decbcf59abae3c0b413c0b4ba64b9b1ebd041b
DaichiRyu <140141915+DaichiRyu@users.noreply.github.com>
2023-07-21 15:15:33 +0900

    Add files via upload

---
+++ proto/register/php/test.php
@@ -0,0 +1,128 @@
+<?php
+sleep(1); // わざと1秒待つ。不必要なら削除する
+
+$ndata = $_POST['名前']; // 送ったデータを受け取る（GETで送った場合は、INPUT_GET）
+$vdata = $_POST['数量']; 
+$foodlist= array();
+$foods = array();
+for($i=0;$i<count($ndata);$i+=1){
+    $food=array("name"=> $ndata[$i],"value"=>$vdata[$i]);
+	array_push($foods, $food);
+}
+foreach($ndata as $name)
+{
+    //配列の内容を1個ずつ書き出す。
+    echo $name;
+}
+ 
+foreach($vdata as $number)
+{
+    //配列の内容を1個ずつ書き出す。
+    echo $number;
+}
+ 
+
+?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登録リスト</title>
+
+    <link rel="stylesheet" href="../css/searchFromJancode.css">
+    <script src="../../js/include/foods.js"></script>
+    <script src="../js/searchFromJancode.js"></script>
+</head>
+
+<body>
+<div id="executing" class="disable">
+        実行中
+    </div>
+
+    <header class="title">
+        <a href="barcoderegister.html"><img src="../images/shape02.png" alt=""></a>
+        <p>登録リスト画面</p>
+        <span>　</span>
+    </header>
+    <main>
+        <div id="foods-container">
+
+            <?php for ($i = 0; $i < count($foods); $i++) { ?>
+            <div class="food">
+                <div class="food-left">
+                    <label for="">食品名</label><input class="food-name editable-input" type="text"
+                        value="<?php echo $foods[$i]["name"]; ?>">
+                </div>
+                <div class="food-right">
+                    <div>
+                        <label for="">個数</label><input class="food-number editable-input" type="number"
+                         value="<?php echo $foods[$i]["value"]; ?>">
+                    </div>
+                    <div>
+                        <label for="">消費期限</label><input class="food-ub editable-input" type="date"
+                            value="<?php print_r(date("Y-m-d")) ?>">
+                    </div>
+                    <div>
+                        <label for="">賞味期限</label><input class="food-bb editable-input" type="date"
+                            value="">
+                    </div>
+                    <div>
+                        <label for="">保存場所</label>
+                        <!--<select name="" class="food-storage select-storage editable-input">
+
+                            <?php #for ($j = 0; $j < count($storage); $j++) { ?>
+
+                            <option value="<?php #echo $storage[$j]["id"] ?>"><?php #echo $storage[$j]["name"] ?></option>
+
+                            <?php #} ?>
+
+                        </select>-->
+                    </div>
+                    <div>
+                        <label for="">ジャンル</label>
+                        <!--<select name="" class="food-genre select-genre editable-input">
+
+                            <?php #for ($j = 0; $j < count($genre); $j++) { ?>
+
+                            <option value="<?php #echo $genre[$j]["id"] ?>"><?php #echo $genre[$j]["name"] ?></option>
+
+                            <?php #} ?>
+
+                        </select>-->
+                    </div>
+                    <div>
+                        <label for="">金額</label><input class="food-money editable-input" type="number" value="0">
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+
+        </div>
+    </main>
+    <footer id="menu-container">
+        <span></span>
+        <div id="button-submit">登録</div>
+        <div id="button-cancel">
+            <a href="../home.php">キャンセル</a>
+        </div>
+    </footer>
+
+    <?php
+    if(count($foods)==0){    
+    ?>
+    <script>
+        //result = window.confirm("食品が検出されませんでした。手入力を行いますか？");
+        if(result){
+            location = "../register.php"
+        }else{
+            location = "../photoregister.html"
+        }
+    </script>
+    <?php
+    }
+    ?>
+</body>
+
+</html>
--- proto/register/php/trp.php
+++ proto/register/php/trp.php
@@ -1,1 +1,50 @@
-
+<?php
+do{
+    $flg = false;
+    //FTPサーバとアカウント情報
+    $cfg['server'] = "sv14359.xserver.jp"; //送り先のFTPサーバー名もしくはIP
+    $cfg['user'] = "stoliter"; //送り先のFTPユーザ
+    $cfg['pass'] = "../.key/stoliterfl.key"; //送り先のFTPパスワード
+    $cfg['remoteDir'] = 'stolitor.xsrv.jp/public_html/php/imgfiles/'; //送り先のディレクトリ
+    $cfg['localDir'] = './tmp/'; //ローカル側の一時アップロードディレクトリ
+    $local_filename = $cfg['localDir'] . $_FILES['imgname']['name']; //アップロードするファイル
+    $remotes_filename = $cfg['remoteDir'].$_FILES['imgname']['name']; //アップロード時の名前
+    echo 0;
+    function uploadSFTP($local_filename, $remote_filename)
+    {   
+        global $cfg;
+        echo 1;
+        //FTPサーバに接続
+        $conn = ssh2_connect($cfg['server'],10022);
+        echo 2;
+        if( !ssh2_sutj_pubkey_file($conn,$cfg['user'],$cfg['pass'])){
+            echo"login failed";
+            return;
+        }
+        echo 3;
+        //ログイン
+        $sftp = ssh2_sftp($conn);
+        echo 4;
+        //ローカル側に一度アップロード
+        if( !move_uploaded_file($_FILES['file']["tmp_name"], $cfg['localDir'] . $_FILES['file']['name']) ) ;
+        echo 5;
+        //アップロード
+        $file_data=file_get_contents($local);
+        if(file_put_contents('ssh.sftp://'.$sftp.$remote,$file_data));
+        echo 6;
+        //ローカル側のファイルを削除
+        unlink( $cfg['localDir'] . $_FILES['file']['name'] );
+        echo 7;
+        //接続を閉じる
+        ssh2_disconnect($conn);
+        echo 8;
+        $flg = true;
+    }
+    echo 9;
+}while(0);
+if( $flg ){
+    //アップロード成功時の処理
+}else{
+    //アップロード失敗時の処理
+}
+?>
